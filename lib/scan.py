22e4b9d8393f2e9fbbb529fad182a1c320bd1a0e
Faiz <61777096+faizdotid@users.noreply.github.com>
2022-10-17 09:38:13 +0700

    Add files via upload

---
+++ lib/scan.py
@@ -0,0 +1,64 @@
+import requests
+import re
+from colorama import Fore, init as colorama_init
+
+
+class RequestSite:
+    def __init__(self, domain):
+        self.domain = 'http://'+domain
+        self.invalid = Fore.RED + 'Unknown'
+        self.valid = Fore.GREEN + '{}'
+
+    def request(self):
+        try:
+            self.response = requests.get(self.domain, headers={
+                                         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, timeout=10, allow_redirects=True)
+        except:
+            self.response = False
+
+    def get_server(self):
+        try:
+            _server = self.valid.format(self.response.headers['Server'])
+        except:
+            _server = self.invalid
+        finally:
+            return _server
+
+    def get_status_code(self):
+        try:
+            _status_code = self.valid.format(str(self.response.status_code))
+        except:
+            _status_code = self.invalid
+        finally:
+            return _status_code
+
+    def get_cms(self):
+        conf = {
+            'WordPress': {
+                'text': 'wp-content',
+                'context': self.response.text
+            },
+            'Drupal': {
+                'text': '/sites/default/files/',
+                'context': self.response.text
+            },
+            'Joomla': {
+                'text': 'com_content',
+                'context': self.response.text
+            },
+            'Laravel': {
+                'text': 'laravel_session|XSRF-TOKEN',
+                'context': self.response.headers
+            },
+        }
+        for key, value in conf.items():
+            if re.search(value['text'], value['context']):
+                return self.valid.format(key)
+            else:
+                return self.invalid
+
+    # def get_domain(self):
+    #     url = self.response.url
+    #     urls = url.split('/')
+    #     _url = urls[0] + '//' + urls[2]
+    #     return _url
---
+++ main.py
@@ -0,0 +1,112 @@
+import requests
+import argparse
+from lib.scan import RequestSite, Fore, colorama_init
+import re
+
+colorama_init(autoreset=True)
+
+
+class Lookup:
+
+    def __init__(self, url):
+        self.url = url
+        self.domain = self.get_root_domain()
+        self.results = list()
+
+    def get_root_domain(self):
+        if '://' in self.url:
+            domain = self.url.split('/')[2]
+        elif self.url.endswith('/'):
+            domain = self.url.split('/')[0]
+        else:
+            domain = self.url
+        return domain
+
+    def request(self, _api):
+        try:
+            resp = requests.get(_api, headers={
+                                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}, timeout=70).text
+            if re.search('error', resp):
+                return False
+            else:
+                return resp
+        except requests.RequestException:
+            print(Fore.RED + "Please Check Ur Connection!!!")
+            return False
+
+    def reverseip(self):
+        api = 'https://api.hackertarget.com/reverseiplookup/?q={}'.format(
+            self.domain)
+        _resp = self.request(api)
+        if _resp:
+            self.results.extend(_resp.splitlines())
+        else:
+            pass
+
+    def subdomain(self):
+        api = 'https://api.hackertarget.com/hostsearch/?q={}'.format(
+            self.domain)
+        _resp = self.request(api)
+        if _resp:
+            self.results.extend([url.split(',')[0]
+                                for url in _resp.splitlines()])
+        else:
+            pass
+
+
+class Main(object):
+    def __init__(self, domain, option):
+        self.target = domain
+        self.option = option
+
+    def _output(self, result_lookup):
+        scanner = RequestSite(result_lookup)
+        scanner.request()
+        _format = '{:<50} - {}'
+        if scanner.response:
+            cms = scanner.get_cms()
+            server = scanner.get_server()
+            http_status_code = scanner.get_status_code()
+            _fp = _format.format('http://'+result_lookup, '{:^25} | {:^30} | {}'.format(
+                cms + Fore.WHITE, http_status_code + Fore.WHITE, server))
+            print(_fp)
+        else:
+            _fp = _format.format('http://'+result_lookup, scanner.invalid)
+
+    def _start(self):
+        try:
+            _fm = "{:^50} - {}"
+            lookup = Lookup(self.target)
+            _getresults = getattr(lookup, self.option)
+            _getresults()
+            if len(lookup.results) > 0:
+                print('Found {} {} in {}\n'.format(
+                    len(lookup.results), self.option, lookup.domain))
+                print(_fm.format("Domain", '{:^20} | {:^25} | {}'.format(
+                    "CMS" + Fore.WHITE, "HTTP Code" + Fore.WHITE, "Server")))
+                for dom in lookup.results:
+                    self._output(dom)
+            else:
+                print('No results found for domain {}'.format(self.target))
+        except KeyboardInterrupt:
+            print(Fore.RED + "CTRL + C Detected\nExiting Tools....")
+            exit()
+
+
+print(Fore.GREEN + """
+█▀▄ █▀█ █▀▄▀█ █▀ █▀▀ ▄▀█ █▄░█
+█▄▀ █▄█ █░▀░█ ▄█ █▄▄ █▀█ █░▀█
+Simple D0main Scanner\n""")
+parser = argparse.ArgumentParser(
+    prog="main", description="Domain Scanner", usage="%(prog)s domain [option]")
+parser.add_argument("-u", "--url", required=True,
+                    type=str, help="Domain to scan")
+parser.add_argument(
+    "-o", "--option", choices=["reverseip", "subdomain"], required=True, help="Option to scan")
+
+args = parser.parse_args()
+
+if args:
+    Main(domain=args.url, option=args.option)._start()
+else:
+    parser.print_help()
